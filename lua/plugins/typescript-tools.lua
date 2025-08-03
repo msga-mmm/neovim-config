7e14403c78af1bc01f944ce7c7f922f67a76da97
msga-mmm <109926431+msga-mmm@users.noreply.github.com>
2025-08-02 20:24:03 -0500

    fix: disable typescript tools since ts ls is also enabled (#10)

--- lua/plugins/typescript-tools.lua
+++ lua/plugins/typescript-tools.lua
@@ -1,21 +1,22 @@
 return {
-    "pmizio/typescript-tools.nvim",
-    dependencies = {
-        "neovim/nvim-lspconfig",
-        "nvim-lua/plenary.nvim",
-    },
-    config = function()
-        require("typescript-tools").setup({
-            settings = {
-                expose_as_code_action = "all",
-            },
-        })
+    -- disabled because it shows double lsp documentation and rename things since ts_ls is also running so they mix up
+    -- "pmizio/typescript-tools.nvim",
+    -- dependencies = {
+    --     "neovim/nvim-lspconfig",
+    --     "nvim-lua/plenary.nvim",
+    -- },
+    -- config = function()
+    --     require("typescript-tools").setup({
+    --         settings = {
+    --             expose_as_code_action = "all",
+    --         },
+    --     })
 
-        vim.api.nvim_create_autocmd({ "BufEnter" }, {
-            pattern = { "*.ts", "*.tsx", "*.js", "*.jsx" },
-            callback = function()
-                vim.cmd("LspStart typescript-tools")
-            end,
-        })
-    end,
+    --     vim.api.nvim_create_autocmd({ "BufEnter" }, {
+    --         pattern = { "*.ts", "*.tsx", "*.js", "*.jsx" },
+    --         callback = function()
+    --             vim.cmd("LspStart typescript-tools")
+    --         end,
+    --     })
+    -- end,
 }
