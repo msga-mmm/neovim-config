bc7c5a2f62ccb50d9a386f648a04d484ac592abc
msga-mmm <michael.sga.mmm@gmail.com>
2026-04-12 19:01:17 -0500

    fix: configure better comments for react

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,10 +1,28 @@
 return {
-    "numToStr/Comment.nvim",
-    opts = {
-        -- pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
-        ignore = "^$",
-    },
-    dependencies = {
+    {
         "JoosepAlviste/nvim-ts-context-commentstring",
+        config = function()
+            require("ts_context_commentstring").setup({
+                enable_autocmd = false,
+            })
+        end,
+    },
+    {
+        "nvim-mini/mini.comment",
+        version = false,
+        keys = {
+            { "gc", mode = { "n", "x" } },
+            { "gbc", mode = { "n", "x" } },
+        },
+        config = function()
+            require("mini.comment").setup({
+                options = {
+                    custom_commentstring = function()
+                        return require("ts_context_commentstring").calculate_commentstring()
+                            or vim.bo.commentstring
+                    end,
+                },
+            })
+        end,
     },
 }
