3e5dd6e22ff75fca03ed5c9ee21ec7572760e512
msga-mmm <109926431+msga-mmm@users.noreply.github.com>
2026-04-12 19:03:02 -0500

    fix: configure better comments for react (#20)

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -14,6 +14,7 @@ return {
         dependencies = { "williamboman/mason.nvim" },
         config = function()
             require("mason-lspconfig").setup({
+                ensure_installed = { "ts_ls" },
                 automatic_installation = true,
             })
         end,
